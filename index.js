58151cdb814db970e0386485477471ec093d0996
BensonGitau <bensonkamaugitau@gmail.com>
2024-03-29 13:18:47 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Your code here
-const dodger = document.getElementById("dodger");
+/*const dodger = document.getElementById("dodger");
 function moveDodgerLeft() {
   const leftNumbers = dodger.style.left.replace("px", "");
   const left = parseInt(leftNumbers, 10);
@@ -26,4 +26,41 @@ document.addEventListener("keydown", function(e) {
     if (e.key === "ArrowRight") {
         moveDodgerRight();
     };
+});
+*/
+
+
+
+const dodger =  document.querySelector('#dodger')
+
+function moveDodgerLeft () {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0){
+    dodger.style.left = `${left - 1}px`;
+  }
+
+}
+
+document.addEventListener('keydown', function (e) {
+  if(e.key === "ArrowLeft"){
+    moveDodgerLeft();
+  }
+});
+
+
+function moveDodgerRight (){
+  const rightNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(rightNumbers, 10);
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+
+}
+
+document.addEventListener('keydown', function (e) {
+  if(e.key === "ArrowRight"){
+    moveDodgerRight();
+  }
 });
